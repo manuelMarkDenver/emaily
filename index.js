3702aab3e54b6aa22a084714538e368fc367f914
manuelMarkDenver <manuel.markdenver@gmail.com>
2022-10-07 09:18:49 +0800

    passportjs done

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 app.use(express.json());
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+    console.log("🚀 ~ file: index.js ~ line 17 ~ profile", profile)
+    console.log("🚀 ~ file: index.js ~ line 17 ~ refreshToken", refreshToken)
+    console.log("🚀 ~ file: index.js ~ line 17 ~ accessToken", accessToken)
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
-})
-
 app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`)
-})
+  console.log(`Listening to port ${PORT}`);
+});
